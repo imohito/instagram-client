da328a8d3e2a0281e2d5e6f938a8de0db17810de
Arnaud Coomans <arnaud.coomans@gmail.com>
2013-07-13 16:25:58 +0200

    Made a package

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+config = {
+	'name': 'instagram-client',
+	'version': '0.1',
+	'description': 'Upload pictures to instagram',
+	'license': 'LICENSE.txt',
+	'url': 'https://bitbucket.org/acoomans/instagram-client',
+	'author': 'Arnaud Coomans',
+	'install_requires': ['requests'],
+	'packages': ['instagram'],
+}
+
+setup(**config)
---
+++ tests/test.py
@@ -0,0 +1,9 @@
+from instagram.client import Instagram
+
+Instagram.KEY = "" # do NOT ask me for the key 
+Instagram.DEVICEID = "00000000-0000-0000-0000-000000000000"
+
+client = Instagram()
+client.login("username", "password")
+media_id = client.upload('test.jpg')
+client.configure(media_id, "My caption")
